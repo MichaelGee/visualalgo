924aa04632dea26263fa7f1f1b6437f4932e5508
sleekvick <nvonweb@outlook.com>
2019-05-25 01:11:59 -0500

    Use webpack middle ware for ssr

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,35 @@
+import mainRouter from '@app/server/routers/main';
+import * as bodyParser from "body-parser";
+import * as morgan from "morgan";
+import * as path from 'path';
+import * as webpack from 'webpack';
+import * as webpackDevMiddleware from 'webpack-dev-middleware';
+
 import * as express from 'express';
+
+const config = require('../../webpack.config.js');
+const compiler = webpack(config[1]);
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Express server');
-});
+app.use(morgan());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use(
+    webpackDevMiddleware(compiler, {
+        historyApiFallback: true,
+        hot: true,
+        publicPath: config[1].output.publicPath,
+    }),
+);
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+console.log(path.join(__dirname, '/static'));
+
+app.use('/static', express.static(path.join(__dirname, '/static')));
+
+app.use(mainRouter);
 
 export default app;
