847c78f77c532b753204101325ea0fa94fa9005c
sleekvick <nvonweb@outlook.com>
2019-05-25 16:30:40 -0500

    Test prime number generation and median

--- src/__tests__/server/server.ts
+++ src/__tests__/server/server.ts
@@ -9,18 +9,54 @@ const request = chai.request;
 describe('Server', () => {
 
     it('Server Side Rendered', (done) => {
-
         request(appServer)
             .get('/')
             .end((err, res) => {
                 if (err) { console.error(err); }
-
                 const {text} = res;
-
                 expect(text).toMatch('Sieve of Eratosthenes');
                 done();
             });
 
+    });
+
+    it('Get Seive', (done) => {
+        request(appServer)
+            .get('/prime/10')
+            .end((err, res) => {
+                if (err) { console.error(err); }
+
+                const {body: { primes } } = res;
+
+                expect(primes).toEqual([2, 3, 5, 7]);
+                done();
+            });
+    });
+
+    it('Get Seive Median: Double', (done) => {
+        request(appServer)
+            .get('/prime/10/median')
+            .end((err, res) => {
+                if (err) { console.error(err); }
+
+                const {body: { median } } = res;
+
+                expect(median).toEqual([3, 5]);
+                done();
+            });
+    });
+
+    it('Get Seive Median: Single', (done) => {
+        request(appServer)
+            .get('/prime/18/median')
+            .end((err, res) => {
+                if (err) { console.error(err); }
+
+                const {body: { median } } = res;
+
+                expect(median).toEqual([7]);
+                done();
+            });
     });
 
 });
--- src/server/routers/api/index.ts
+++ src/server/routers/api/index.ts
@@ -2,9 +2,47 @@ import * as express from 'express';
 
 const apiRouter = express.Router();
 
+const getPrimes = (max: number): number[] => {
+    const sieve = new Array(max).fill(true);
+
+    for (let i = 2; i < Math.sqrt(max); i++) {
+        if (sieve[i]) {
+            for (let j = Math.pow(i, 2); j < max; j += i) {
+                sieve[j] = false;
+            }
+        }
+    }
+
+    return sieve.reduce((primes, isPrime, i) => {
+        if (isPrime && i > 1) {
+            primes.push(i);
+        }
+
+        return primes;
+    }, []);
+};
+
+const getMedian = (arr: number[]): number[] => {
+    const mid = Math.floor(arr.length / 2);
+
+    if (arr.length % 2 === 0) {
+        return [arr[mid - 1], arr[mid]];
+    } else {
+        return [arr[mid]];
+    }
+};
+
 apiRouter.get('/prime/:limit', (req, res) => {
     const { limit } = req.params;
-    res.send(limit);
+    const primes = getPrimes(Number(limit));
+    res.json({ primes });
+});
+
+apiRouter.get('/prime/:limit/median', (req, res) => {
+    const { limit } = req.params;
+    const primes = getPrimes(Number(limit));
+    const median = getMedian(primes);
+    res.json({ median });
 });
 
 export default apiRouter;
