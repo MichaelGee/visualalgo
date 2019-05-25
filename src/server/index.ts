a6eec5c661c783cd3263464dfd137895d60f01aa
sleekvick <nvonweb@outlook.com>
2019-05-25 15:26:56 -0500

    Only hot when on dev

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,31 +2,34 @@ import apiRouter from '@app/server/routers/api';
 import homeRouter from '@app/server/routers/home';
 
 import * as bodyParser from 'body-parser';
-import * as morgan from 'morgan';
+import morgan from 'morgan';
 import * as path from 'path';
-import * as webpack from 'webpack';
-import * as webpackDevMiddleware from 'webpack-dev-middleware';
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
 
-import * as express from 'express';
+const express = require('express');
 
 const config = require('../../webpack.config.js');
 const compiler = webpack(config[1]);
 
 const app = express();
 
-app.use(morgan());
+app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(
-    webpackDevMiddleware(compiler, {
-        historyApiFallback: true,
-        hot: true,
-        publicPath: config[1].output.publicPath,
-    }),
-);
+if (process.env.NODE_ENV === 'dev') {
+    app.use(
+        webpackDevMiddleware(compiler, {
+            historyApiFallback: true,
+            hot: true,
+            publicPath: config[1].output.publicPath,
+        }),
+    );
 
-app.use(require('webpack-hot-middleware')(compiler));
+    app.use(webpackHotMiddleware(compiler));
+}
 
 app.use('/static', express.static(path.join(__dirname, '/static')));
 
