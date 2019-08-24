9d3348ad9edd1d47abc6d0a842f444df10d91885
Nwaokocha Victor <nvonweb@outlook.com>
2019-08-24 05:56:46 -0500

    Implement Kadanes algorithm

--- src/__tests__/server/server.ts
+++ src/__tests__/server/server.ts
@@ -52,9 +52,26 @@ describe('Server', () => {
             .end((err, res) => {
                 if (err) { console.error(err); }
 
-                const {body: { median } } = res;
+                const { body: { median } } = res;
 
                 expect(median).toEqual([7]);
+                done();
+            });
+    });
+
+    it('Get Maximum sub array', (done) => {
+        request(appServer)
+            .post('/max_subarray')
+            .send({
+                items: [ 1, -3, 2, 1, -1],
+            })
+            .end((err, res) => {
+                if (err) { console.error(err); }
+
+                const { body: { items, max } } = res;
+
+                expect(items).toEqual([2, 1]);
+                expect(max).toEqual(3);
                 done();
             });
     });
--- src/server/routers/api/index.ts
+++ src/server/routers/api/index.ts
@@ -32,6 +32,31 @@ const getMedian = (arr: number[]): number[] => {
     }
 };
 
+function getMaximumSubArr(arr) {
+    let globalMax = arr[0];
+    let localMax = arr[0];
+
+    let startIndex = 0;
+    let endIndex = arr.length - 1;
+
+    for (let i = 1; i < arr.length - 1; i++) {
+        const sum = localMax + arr[i];
+        localMax = Math.max(sum, arr[i]);
+
+        if (localMax > globalMax) {
+            globalMax = localMax;
+
+            if (globalMax === sum) {
+                endIndex = i;
+            } else {
+                startIndex = i;
+            }
+        }
+    }
+
+    return { max: globalMax, items: arr.slice(startIndex, endIndex + 1) };
+}
+
 apiRouter.get('/prime/:limit', (req, res) => {
     const { limit } = req.params;
     const primes = getPrimes(Number(limit));
@@ -43,6 +68,12 @@ apiRouter.get('/prime/:limit/median', (req, res) => {
     const primes = getPrimes(Number(limit));
     const median = getMedian(primes);
     res.json({ median, primes });
+});
+
+apiRouter.post('/max_subarray', (req, res) => {
+    const { items: arr } = req.body;
+    const { items, max } = getMaximumSubArr(arr);
+    res.json({ items, max });
 });
 
 export default apiRouter;
